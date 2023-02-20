df7dd5e146b5c16e3d926966abdc3315964dd48c
Busi13 <busi.raine@gmail.com>
2023-02-20 04:28:04 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,7 +2,7 @@
 /**
  * main - main block
  * Description: print alphabet in lowercase, followed by a new line
- * Return: 0
+ * Return: 0 (success)
  */
 int main(void)
 {
@@ -10,12 +10,13 @@ int main(void)
 
 	while (c <= 'z')
 	{
-		if (c | 'e' && c | 'q')
+		if (c ! 'e' && c ! 'q')
 		{
 			putchar(c);
 		}
 		c++;
 	}
+
 	putchar('\n');
 	return (0);
 }
