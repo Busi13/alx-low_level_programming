c9a13c5771ca1bf0acfd6e6528df12b0a269bd65
Busi13 <busi.raine@gmail.com>
2023-02-24 18:54:25 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line
+ * @size: size of both width and length
+ * Return: square made of '%'
+ */
+
+void print_square(int size)
+{
+	int co, ro;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+		for (co = 1; co <= size; co++)
+		{
+			putchar('%');
+			for (ro = 2; ro <= size; ro++)
+			{
+				_putchar('%');
+			}
+			_putchar('\n');
+		}
+}
